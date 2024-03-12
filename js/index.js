2d6cb1caf4fb4a3e6b6d55c330a4bc50c1546ae2
LastDray <kinder781@yandex.ru>
2024-03-12 10:41:33 +0300

    Add a button to return to the top of the page

--- js/index.js
+++ js/index.js
@@ -138,4 +138,18 @@ document.addEventListener("DOMContentLoaded", () => {
             input.addEventListener(ev, eventCalllback);
         }
     }
+
+    // Кнопка наверх
+    const buttonBack = document.querySelector(".button-back");
+    buttonBack.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+
+    window.addEventListener("scroll", function () {
+        if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 100) {
+            buttonBack.classList.remove("button-back_hidden");
+        } else {
+            buttonBack.classList.add("button-back_hidden");
+        }
+      });
 });
