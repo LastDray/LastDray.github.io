5354af66f6db40339ac89326a7524451cc89400f
LastDray <kinder781@yandex.ru>
2024-03-12 09:31:03 +0300

    Add validation and mask for the number

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Модальное окно с локацией
     const locationDialog = document.querySelector("#location");
     const locationButton = document.querySelector(".location");
     const locationItems = document.querySelectorAll(".location-modal__item");
@@ -48,6 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    // Модальное окно с формой
     const formDialog = document.querySelector(".form-modal");
     const formButtonOpen = document.querySelector(".benefit__block-action .btn");
     const formButtonSubmit = document.querySelector(".form-modal .instructions__form-action .btn");
@@ -61,4 +63,29 @@ document.addEventListener("DOMContentLoaded", () => {
         formDialog.querySelector("form").reset();
         document.body.classList.remove("scroll-lock");
     });
+
+    // Маска для номера
+    const telInputs = document.querySelectorAll('input[type="tel"]');
+    const eventCalllback = function (e) {
+        let clearVal = 'false',
+        matrix = "+7 (___) ___-__-__",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = e.target.value.replace(/\D/g, "");
+        if (clearVal !== 'false' && e.type === 'blur') {
+            if (val.length < matrix.match(/([\_\d])/g).length) {
+                e.target.value = '';
+                return;
+            }
+        }
+        if (def.length >= val.length) val = def;
+        e.target.value = matrix.replace(/./g, function (a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+        });
+    }
+    for (input of telInputs) {
+        for (let ev of ['input', 'blur', 'focus']) {
+            input.addEventListener(ev, eventCalllback);
+        }
+    }
 });
