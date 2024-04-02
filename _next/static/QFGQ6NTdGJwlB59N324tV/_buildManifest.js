38f1bd185378c936476ca58f048ac61a30bd981b
LastDray <kinder781@yandex.ru>
2024-04-02 15:45:54 +0300

    add video

---
+++ _next/static/QFGQ6NTdGJwlB59N324tV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/151-19ad2b0dba5be54d.js","static/css/338b7c277a145d81.css","static/chunks/pages/index-f447500fd924395f.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
